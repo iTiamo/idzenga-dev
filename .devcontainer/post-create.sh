482d39ba77ac1e68dd7308fbdca936a0cd495f2c
Tiamo Idzenga <tiamo@idzenga.dev>
2024-11-04 08:33:33 +0000

    install dependencies in post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,2 +1,8 @@
 # Install Ruby for Rusy LSP plugin
 rvm install "ruby-$(<.ruby-version)"
+
+# Install dependencies for project
+bundle install
+
+# Run project
+# bundle exec jekyll serve --force_polling
